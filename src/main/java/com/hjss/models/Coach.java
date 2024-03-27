47a0269bf1a645533b744752d718c671541fb07a
asifmanan <asifmanan@gmail.com>
2024-03-27 23:29:40 +0000

    Coach randomIdentifier feature set

--- src/main/java/com/hjss/models/Coach.java
+++ src/main/java/com/hjss/models/Coach.java
@@ -8,6 +8,7 @@ import java.time.Year;
 
 public class Coach extends Person{
     String coachId;
+    int randomIdentifier;
     public Coach(String firstName, String lastName, Gender gender, LocalDate dateOfBirth) {
         super(firstName, lastName, gender, dateOfBirth);
         generateId();
@@ -15,13 +16,20 @@ public class Coach extends Person{
     public Coach(String firstName, String lastName, Gender gender) {
         this(firstName, lastName, gender, null);
     }
+    public Coach(String firstName, String lastName){
+        this(firstName, lastName, Gender.Unknown, null);
+    }
     private void generateId(){
         int currentYear = Year.now().getValue() % 100;
         int sequenceNumber = IdGenerator.generateSequentialId(this.getClass());
+        this.randomIdentifier = sequenceNumber;
         this.coachId = "CH" + String.format("%02d%03d", currentYear, sequenceNumber);
     }
 
     public String getCoachId() {
         return this.coachId;
+    }
+    public int getRandomIdentifier(){
+        return this.randomIdentifier;
     }
 }
