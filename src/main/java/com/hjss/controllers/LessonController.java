920d0ebfa00be20bedbf6dde85452e232a9e162a
asifmanan <asifmanan@gmail.com>
2024-03-30 01:51:56 +0000

    Created the LessonController class

---
+++ src/main/java/com/hjss/controllers/LessonController.java
@@ -0,0 +1,23 @@
+package com.hjss.controllers;
+
+import com.hjss.dataregistry.ModelRegister;
+import com.hjss.models.Lesson;
+
+import java.util.List;
+
+public class LessonController implements ModelController<Lesson> {
+    private final ModelRegister<Lesson> lessonRegister;
+
+    public LessonController() {
+        this.lessonRegister = new ModelRegister<>();
+    }
+
+    @Override
+    public String addObject(Lesson lesson) {
+        return lessonRegister.add(lesson);
+    }
+    @Override
+    public List<Lesson> getAllObjects() {
+        return null;
+    }
+}
