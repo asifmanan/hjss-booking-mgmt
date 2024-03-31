2013a203105fb9ada193535fe3eba2aaa14b62ab
asifmanan <asifmanan@gmail.com>
2024-03-31 22:53:48 +0100

    TimeSlotController created

---
+++ src/main/java/com/hjss/controllers/TimeSlotController.java
@@ -0,0 +1,23 @@
+package com.hjss.controllers;
+
+import com.hjss.modelrepository.ModelRegister;
+import com.hjss.models.WeekDayTimeSlot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeSlotController implements ModelController<WeekDayTimeSlot> {
+    private final ModelRegister<WeekDayTimeSlot> timeSlotRegister;
+    public TimeSlotController(){
+        this.timeSlotRegister = new ModelRegister<WeekDayTimeSlot>();
+    }
+    @Override
+    public String addObject(WeekDayTimeSlot object) {
+        return timeSlotRegister.add(object);
+    }
+
+    @Override
+    public List<WeekDayTimeSlot> getAllObjects() {
+        return new ArrayList<>(timeSlotRegister.getAllObjects());
+    }
+}
