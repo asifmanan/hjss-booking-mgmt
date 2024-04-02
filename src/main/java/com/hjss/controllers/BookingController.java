c3e9cae9011fce9774b22f7486d4a217ac8fd425
asifmanan <asifmanan@gmail.com>
2024-04-02 15:18:25 +0100

    Updated the Booking and Booking Controller classes

--- src/main/java/com/hjss/controllers/BookingController.java
+++ src/main/java/com/hjss/controllers/BookingController.java
@@ -1,4 +1,25 @@
 package com.hjss.controllers;
 
-public class BookingController {
+import com.hjss.modelrepository.ModelRegister;
+import com.hjss.models.Booking;
+
+import java.util.List;
+
+public class BookingController implements ModelController<Booking> {
+    private final ModelRegister<Booking> bookingRegister;
+    public BookingController(){
+        this.bookingRegister = new ModelRegister<>();
+    }
+    public Booking createObject(String learnerId, String lessonId){
+        return new Booking(learnerId, lessonId);
+    }
+    @Override
+    public String addObject(Booking object) {
+        return bookingRegister.add(object);
+    }
+
+    @Override
+    public List<Booking> getAllObjects() {
+        return null;
+    }
 }
