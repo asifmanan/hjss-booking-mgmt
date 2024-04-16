c6f8413bc221e31b44a09e8c52cac88b882e0ce8
asifmanan <asifmanan@gmail.com>
2024-04-17 00:26:30 +0100

    MonthlyLearnerReportView is complete

--- src/main/java/com/hjss/views/MonthlyLearnerReportView.java
+++ src/main/java/com/hjss/views/MonthlyLearnerReportView.java
@@ -4,14 +4,19 @@ import com.hjss.controllers.BookingController;
 import com.hjss.controllers.LearnerController;
 import com.hjss.models.Booking;
 import com.hjss.models.Learner;
+import com.hjss.utilities.BookingStatus;
+import com.hjss.utilities.InputValidator;
 import com.hjss.utilities.Pair;
 import com.hjss.utilities.TablePrinter;
 import io.consolemenu.TerminalManager;
 import org.jline.reader.LineReader;
 import org.jline.terminal.Terminal;
+import org.jline.utils.InfoCmp;
 
 import java.io.IOException;
+import java.time.Month;
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class MonthlyLearnerReportView {
     private BookingController bookingController;
@@ -20,6 +25,7 @@ public class MonthlyLearnerReportView {
     private TablePrinter tablePrinter;
     private int tableWidth;
     private List<String> lessonListHeader;
+    private List<String> bookingSummaryHeader;
     private Map<String, Integer> lessonListColWidth = new HashMap<>();
     public MonthlyLearnerReportView(BookingController bookingController, LearnerController learnerController){
         this.bookingController = bookingController;
@@ -32,6 +38,10 @@ public class MonthlyLearnerReportView {
                                                 "LessonGrade",
                                                 "Coach"
                                                 );
+        this.bookingSummaryHeader = Arrays.asList(  "Booked Lessons",
+                                                    "Cancelled Lessons",
+                                                    "Attended Lessons"
+                                                    );
         setLessonListColWidth();
         updateTableWidth();
         this.tablePrinter = new TablePrinter(this.lessonListHeader,this.lessonListColWidth);
@@ -74,26 +84,79 @@ public class MonthlyLearnerReportView {
         return bookingData;
     }
 
-    public void printLessonListByLearner(Learner learner){
+    public void printLessonListByLearner(Learner learner, int month){
         List<Booking> bookingListByLearner = bookingController.getBookingsByLearner(learner);
+        List<Booking> monthlyLearnerBookingList = bookingListByLearner.stream().
+                filter(booking -> booking.getLesson().getWeekDayTimeSlot().getMonth()==month).toList();
+        if(monthlyLearnerBookingList.isEmpty()) return;
         System.out.print("\n");
         printLearnerInfo(learner);
-        if(bookingListByLearner.isEmpty()){
-            System.out.println("\n" + " ".repeat(3)+"<No bookings found>"+"\n");
-            return;
-        }
+        printBookingSummary(monthlyLearnerBookingList);
+//        if(monthlyLearnerBookingList.isEmpty()){
+//            System.out.println("\n" + " ".repeat(3)+"<No bookings found>"+"\n");
+//            return;
+//        }
         printLessonListHeader();
-        for(Booking booking : bookingListByLearner){
+        for(Booking booking : monthlyLearnerBookingList){
             List<String> bookingData = getBookingData(booking);
             tablePrinter.printRow(bookingData);
         }
-        System.out.print("\n");
+        System.out.println(" ".repeat(3) + "-".repeat(tableWidth)+"\n");
+    }
+    public Integer getMonthFromInput(){
+        String leftMargin = " ".repeat(3);
+        try {
+            Terminal terminal = TerminalManager.getTerminal();
+            LineReader lineReader = TerminalManager.getLineReader();
+            String prompt = "Month: ";
+            String regex = "^(1[0-2]|[1-9])$";
+            terminal.writer().println(leftMargin+"Enter Month (Number) to view Monthly Report");
+            List<String> monthList = IntStream.rangeClosed(1,12).mapToObj(String::valueOf).toList();
+            TerminalManager.updateCompleter(monthList);
+            String month = InputValidator.getAndValidateString(terminal,lineReader,prompt,regex);
+            if(month == null) return null;
+            return Integer.parseInt(month);
+        } catch (IOException e){
+            return null;
+        }
     }
     public void printLearnerReport(){
         updateLearnerList();
-        for(Learner learner : learnerList){
-            printLessonListByLearner(learner);
+        int month = getMonthFromInput();
+        List<Booking> bookingListByMonth = bookingController.getAllObjects().stream().
+                filter(booking -> booking.getLesson().getWeekDayTimeSlot().getMonth()==month).toList();
+
+        if(bookingListByMonth.isEmpty()){
+            System.out.println(" ".repeat(3) + "No data exists for the selected Month");
+            return;
         }
+        try {
+            Terminal terminal = TerminalManager.getTerminal();
+            terminal.puts(InfoCmp.Capability.clear_screen);
+        } catch (IOException e){
+            return;
+        }
+        System.out.println(" ".repeat(3) + "=".repeat(tableWidth));
+        System.out.println(" ".repeat(3) + "LEARNER REPORT");
+        System.out.println(" ".repeat(3) + "=".repeat(tableWidth)+"\n");
+        for(Learner learner : learnerList){
+            printLessonListByLearner(learner, month);
+        }
+    }
+    private void printBookingSummary(List<Booking> monthlyLearnerBookingList){
+        int activeLessons = (int) monthlyLearnerBookingList.stream().
+                filter(booking -> booking.getBookingStatus() == BookingStatus.Active).count();
+        int cancelledLessons = (int) monthlyLearnerBookingList.stream().
+                filter(booking -> booking.getBookingStatus() == BookingStatus.Cancelled).count();
+        int attendedLessons = (int) monthlyLearnerBookingList.stream().
+                filter(booking -> booking.getBookingStatus() == BookingStatus.Attended).count();
+        int bookedLessons = activeLessons + cancelledLessons + attendedLessons;
+
+        String bookingSummary = String.format("Booked: %d    Active: %d    Cancelled: %d    Attended: %d    ",
+                bookedLessons, activeLessons, cancelledLessons, attendedLessons);
+        System.out.print(" ".repeat(3)+"Booking Summary  ->  ");
+        System.out.println(bookingSummary);
+        System.out.println(" ".repeat(3)+"-".repeat(tableWidth));
     }
     private void printLearnerInfo(Learner learner){
         String leftMargin = " ".repeat(3);
@@ -101,10 +164,7 @@ public class MonthlyLearnerReportView {
             Terminal terminal = TerminalManager.getTerminal();
             LineReader lineReader = TerminalManager.getLineReader();
             String learnerInfo =leftMargin + "Learner Name: "+learner.getFormattedFullName()
-                    + " ".repeat(5) + "Learner ID: "+learner.getId() ;
-            int learnerInfoLength = learnerInfo.length();
-            terminal.writer().println(leftMargin + "=".repeat(tableWidth));
-            terminal.writer().println(leftMargin + "Learner Report");
+                    + " ".repeat(5) + "Learner ID: "+learner.getId();
             terminal.writer().println(leftMargin + "-".repeat(tableWidth));
             terminal.writer().println(learnerInfo);
             terminal.writer().println(leftMargin + "-".repeat(tableWidth));
