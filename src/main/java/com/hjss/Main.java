92e64eae5fcd0423ca43958403f87e3642ae2e36
asifmanan <asifmanan@gmail.com>
2024-04-21 23:29:42 +0100

    Refactoring, renamed the HjssAppliation to Main and moved it inside the com.hjss package

--- src/main/java/com/hjss/Main.java
+++ src/main/java/com/hjss/Main.java
@@ -1,6 +1,8 @@
+package com.hjss;
+
 import com.hjss.views.EntryView;
 
-public class HjssApplication {
+public class Main {
     public static void main(String[] args) {
         EntryView entryView = new EntryView();
         entryView.initializeMenu();
